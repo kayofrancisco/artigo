bb4aba24fe570042fd3f63252e3bca8b435cdfd3
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2023-08-14 19:31:46 -0300

    Alterado chave para realizar mais requisições

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
   url: 'https://newsapi.org/v2/everything?q=Apple&sortBy=popularity',
-  apiKey: 'f0ed9005fbd9456389e52d45badf07ae'
+  apiKey: 'cb05d05eaeff423f8aa79b0d8c145471'
+  // apiKey: 'f0ed9005fbd9456389e52d45badf07ae'
 };
