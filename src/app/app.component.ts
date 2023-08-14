8d92c882aa0b8c7c48a1eb8896900df00f77a2a4
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2023-08-14 19:16:16 -0300

    Inserido artigo estático

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,22 @@ import { MessageService } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-  artigos: Artigo[] = [];
-  totalArtigos = 0;
+  artigos: Artigo[] = [
+    {
+      author: 'MarketWatch Automation',
+      content: 'Shares of Workhorse Group Inc. WKHS, -4.50% slipped 4.50% to $0.86 Monday, on what proved to be an all-around great trading session for the stock market, with the NASDAQ Composite Index COMP… [+747 chars]',
+      description: 'Shares of Workhorse Group Inc. slipped 4.50% to $0.86 Monday, on what proved to be an all-around great trading session for the stock market, with the NASDAQ...',
+      publishedAt: '2023-08-14T21:38:47Z',
+      source: {
+          id: 2,
+          name: "MarketWatch"
+        },
+      title: 'Company Close Updates: Workhorse Group Inc. stock underperforms Monday when compared to competitors',
+      url: '"https://www.marketwatch.com/data-news/workhorse-group-inc-stock-underperforms-monday-when-compared-to-competitors-3e488c4-8c8113957e60',
+      urlToImage: '"https://images.mktw.net/im-220105/social'
+    }
+  ];
+  totalArtigos = 1;
   primeiroArtigo = 0;
   travarTela = false;
 
@@ -43,7 +57,7 @@ export class AppComponent implements OnInit {
             detail: err.error.message
           });
           this.primeiroArtigo = 0;
-          this.buscarArtigos(1);
+          // this.buscarArtigos(1);
           this.travarTela = false;
         }
       }
